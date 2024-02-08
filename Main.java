e111c3bfabf8733dc62344a54a2678a05748c565
Igor <ygarik@mail.ru>
2024-02-08 22:31:00 +0300

    сделал проверку всех полей, нашел улучшенный вариант проверки даты

--- Main.java
+++ Main.java
@@ -1,39 +1,90 @@
 package Homework_3;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.LinkedList;
-import java.util.List;
+
+
+
 
 public class Main {
-    
 
-    public static void main(String[] args) {
-        String input = "Воробьев Игорь Александрович 28.09.1984 1488 m";
+    public static void main(String[] args) throws ParseException {
+        String input = "28.09.1984 Воробьев M Игорь Александрович 1488";
         String[] list = input.split(" ");
-        List<String> arrInput = new LinkedList<>();
-        for (String s : list) {
-            arrInput.add(s);
-
-        }
-        System.out.println(arrInput);
-        if (arrInput.size() != 6){
+        
+        System.out.println(Arrays.toString(list));
+        
+        if (list.length != 6) {
             throw new RuntimeException("Введены некорректные данные");
         }
-        String date = "28.09.84";
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        try {
-            Date birthday = (Date) df.parse(date);
-            System.out.println(birthday);
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        
+        String[] resultList = new String[6];
+        for (int i = 0; i < list.length; i++) {
+            try {
+                Date birthday = (Date) df.parse(list[i]);
+                System.out.println(birthday);
+                // String dateOfBirth = list[i];
+                // System.out.println(dateOfBirth);
+                resultList[3] = list[i];
+                list[i] = null;
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                // e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < list.length; i++) {
+            try {
+                int number = Integer.parseInt(list[i]);
+                resultList[4] = list[i];
+                list[i] = null;
+                
 
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+        if (resultList[4] == null){
 
+        }
+        for (int i = 0; i < list.length; i++) {
+            try {
+                if (list[i].toLowerCase().equals("m") || list[i].toLowerCase().equals("f")) {
+                    resultList[5] = list[i];
+                    list[i] = null;
+                }
+
+            } catch (Exception e) {
+            }
+
+            
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < list.length; k++) {
+
+                if (list[k] != null) {
+                    resultList[i] = list[k];
+                    list[k] = null;
+                    break;
+
+                }
+
+            }
+        }
+        for (int i = 0; i < resultList.length; i++) {
+            if(resultList[i] == null){
+                
+                    throw new RuntimeException("Введены некорректные данные");
+               
+            }
+        }
+
+        System.out.println(Arrays.toString(resultList));
+        System.out.println(Arrays.toString(list));
     }
 
 }
